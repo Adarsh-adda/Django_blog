5b225b16e330bbde51bdd7c4e6bf9a7028d629f4
Adarsh-adda <adarshku123654@gmail.com>
2022-01-01 01:45:26 +0530

    Updated settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-z%a1+y2gtwnok*5r4&5u(q#o^^*nb)5e7-eqngy(^o9pr-jje+'
-SECRET_KET = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
