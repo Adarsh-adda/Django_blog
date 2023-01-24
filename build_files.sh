a12d3f468b7163bef5f7688b15fd1c55d1918e4c
Adarsh-adda <adarshku123654@gmail.com>
2023-01-25 02:31:57 +0530

    up

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
-python 3.9 -m pip install -r requirements.txt
-python 3.9 manage.py collectstatic --noinput --clear
+python 3.7 -m pip install -r requirements.txt
+python 3.7 manage.py collectstatic --noinput --clear
 echo " BUILD END"
 
 
